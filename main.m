ddeab69231e1c795bb53097dd8895c3062ac229b
sonderrj <1579014303@qq.com>
2021-12-02 21:56:55 +0800

    style<none>: add code for generating sparse matrix into main.m

--- main.m
+++ main.m
@@ -3,10 +3,22 @@ clear; clc; close all
 disp('start GMRES.');
 
 % Ax = b
-A = pascal(4);  % A is a pascal matrix.
-b = [0 0 0 0]';
-x0 = [1 0 0 0]';    % x0 is the initial sovler vector for Ax = b.
-restart_m = 4;
+% A = pascal(4);  % A is a pascal matrix.
+% b = [0 0 0 0]';
+% x0 = [1 0 0 0]';    % x0 is the initial sovler vector for Ax = b.
+% restart_m = 4;
+
+% Ax = b
+% A = [1,2,3,4;5,6,7,8;9,10,11,12;13,14,15,16];  % A is a pascal matrix.
+% b = [10 26 42 58]';
+% x0 = [1 0 0 0]';    % x0 is the initial sovler vector for Ax = b.
+% restart_m = 4;
+
+A = sprandsym(10,0.7);
+A
+b = [0 0 0 0 0 0 0 0 0 0]';
+x0 = [1 0 0 0 0 0 0 0 0 0]';
+restart_m = 10;
 
 % Arnoldi iterative process.
 % Input: restart_m is the restart parameter
