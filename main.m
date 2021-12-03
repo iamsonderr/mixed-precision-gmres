0dd82c4440959bbcb25ef1c0fdc74d32cce7140c
sonderrj <1579014303@qq.com>
2021-12-03 22:31:23 +0800

    add the outer iteration into main.m

--- main.m
+++ main.m
@@ -15,42 +15,44 @@ disp('start GMRES.');
 % restart_m = 4;
 
 A = sprandsym(10,0.7);
-A
 b = [0 0 0 0 0 0 0 0 0 0]';
 x0 = [1 0 0 0 0 0 0 0 0 0]';
 restart_m = 10;
+tol = 1e-6;% specified accuracy radio 
 
-% Arnoldi iterative process.
-% Input: restart_m is the restart parameter
-[Vm,Hm_bar] = Arnoldi(A,b,x0,restart_m);
+while true
 
-% Givens rotation to hessenberg matrix and /
-% beta * e1 from Arnoldi process, making /
-% hessenberg matrix into upper triangular matrix.
-% Input: be is beta * e1
-%        H is hessenberg matrix
-% Output: T is the matrix after Givens rotation /
-%               hessenberg matrix
-%         bk is the vector after Givens ritation /
-%               to beta * e1
-r0 = b-A*x0;
-beta = norm(r0);
-[~,real_m] = size(Hm_bar);
-beta_e1 = zeros(real_m+1,1);beta_e1(1) = beta;
-[Rm_bar,gm_bar] = Givens( Hm_bar,beta_e1 );
-%Resize Rm_bar and gm_bar
-Rm = Rm_bar(1:real_m,1:real_m);
-gm = gm_bar(1:real_m);
+    % Arnoldi iterative process.
+    % Input: restart_m is the restart parameter
+    [Vm,Hm_bar] = Arnoldi(A,b,x0,restart_m);
 
-% y is the shift from the initial x0.
-y1 = inv(Rm)*gm;% solve directly
-y2 = BackwardUpperTriangular( Rm,gm );% solve backward
+    r0 = b-A*x0;
+    beta = norm(r0);
+    [~,real_m] = size(Hm_bar);
+    beta_e1 = zeros(real_m+1,1);beta_e1(1) = beta;
+    [Rm_bar,gm_bar] = Givens( Hm_bar,beta_e1 );
+    %Resize Rm_bar and gm_bar
+    Rm = Rm_bar(1:real_m,1:real_m);
+    gm = gm_bar(1:real_m);
 
-% x = x0 + V * y
-realSolution = inv(A)*b;
-solution1 = x0+Vm*y1;
-solution2 = x0+Vm*y2;
+    % y is the shift from the initial x0.
+    % ym = inv(Rm)*gm;% solve directly
+    ym = BackwardUpperTriangular( Rm,gm );% solve backward
 
+    % x = x0 + V * y
+    % real_solution = inv(A)*b;
+    % xm = x0+Vm*ym;
+    xm = x0+Vm*ym;
+    xm
+    % judge whether to restart
+    rm = norm(b-A*xm);
+    if rm/beta <= tol
+        break;
+    end
+    
+    % The specified accuary was not achieved, meet the condition to restarting
+    x0 = xm;
+end
 %% test part
 clear; clc; close all
 A = [1,1,1;0,1,1;0,0,1];
